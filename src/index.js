c5339af3ef97aaa2ee1ad05e6d4df4bc18e482ef
MCFrank16 <boonatechnologies@gmail.com>
2020-09-18 18:27:14 +0200

    using data structures

--- src/index.js
+++ src/index.js
@@ -1,83 +1,59 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { v4 as uuid} from 'uuid';
 import { ApolloServer, gql } from 'apollo-server-express';
+
+import userClass from './user';
 
 const app = express();
 app.use(cors());
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Cyuzuzo'
-  },
-  2: {
-    id: '2',
-    username: 'Frank'
-  },
-  3: {
-    id: '3',
-    username: 'Mutabazi'
-  }
-};
+let users = {};
 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-  }
-}
+let messages = [];
+
 
 const schema = gql`
-    type Query {
-      me: User
-      user(id: ID!): User
-      users: [User!]
+   input UserInput {
+     firstname: String,
+     lastname: String,
+   }
 
-      messages: [Message!]!,
-      message(id: ID!): Message!
-    }
+   type User {
+     userId: ID,
+     firstname: String,
+     lastname: String,
+   }
 
-    type User {
-      id: ID!
-      username: String!,
-    }
+   type Query {
+     getUser(id: ID!): User!,
+     getUsers: [User!]
+   }
 
-    type Message {
-      id: ID!,
-      text: String!
-      user: User!
-    }
+   type Mutation {
+     createUser(input: UserInput): User!
+   }
+
+   
 `;
 const resolvers = {
   Query: {
-    user: (parent, { id }) => {
-      return users[id]
+    getUsers: () => {
+      return users;
     },
-    me: (parent, args, { me }) => {
-      return me;
-    },
-    users: () => {
-      return Object.values(users);
-    },
-    messages: () => {
-      return Object.values(messages);
-    },
-    message: (_, { id }) => {
-      return messages[id]
-    },
-    Message: {
-      user: (_, { me }) => {
-        return me;
-      }
-    }
   },
-  User: {
-    username: user => `${user.firstname} ${user.lastname}`
+
+  Mutation: {
+
+    createUser: (input) => {
+      const id = uuid();
+      const newUser = new userClass().addUser(id, input);
+      
+      users[id] = newUser
+
+      return newUser;
+    }
   }
 };
 
---
+++ src/user.js
@@ -0,0 +1,14 @@
+const uuid  = require('uuid').v4();
+
+class Users {
+    constructor (){
+      this.collection = [];
+      this.count = 0;
+    }
+  
+    
+}
+let newUser = new Users();
+
+
+module.exports = Users;
